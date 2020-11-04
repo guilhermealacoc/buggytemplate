75658523646fbad3c0e95c1d8bdf0d5df43bdb2c
Guilherme Alacoc <guilhermealacoc@gmail.com>
2020-11-04 20:03:16 -0300

    adicionando express

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
-const express = require('express');
-const app = express();
+const express = require('./src/app');
 
-app.use('/', (req, res) => {
-    res.send('hello world');
-})
 
 app.listen(4000, (err) =>{
     if(err) {
         return console.log('erro')
     }
     console.log('iniciou em http://localhost:4000')
-})
+});
